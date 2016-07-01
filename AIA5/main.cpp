a732b97134792825823076dce9052259e5b721ed
Sam Gleeson <Sam@sams-macbook-pro.fritz.box>
2016-07-01 13:40:36 +0200

    Initial Commit

---
+++ AIA5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AIA5
+//
+//  Created by Sam Gleeson on 1/07/2016.
+//  Copyright © 2016 Sam Gleeson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
